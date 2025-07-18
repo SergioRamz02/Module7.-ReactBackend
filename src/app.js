24a745f8f01dbf8ee86e33a5254d814d6b82ca16
Sergio Ramírez <sergio_esfm02@hotmail.com>
2025-07-17 23:16:27 -0600

    feat: Correcciones de código para funcionamiento

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const postRoutes = require('./routes/post.routes')
 
 const app = express(); //Trabaja como servidor
-app.use(express.json);
+app.use(express.json());
 
 //Registrando rutas
 app.use('/post', postRoutes); //Endpoint = http://localhost:3000/post
